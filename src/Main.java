b7d1f785e6e0ab5addd853bbfcbcd425780c8733
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-12 11:54:14 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by KPS on 7/12/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+            String main_string = "Python";
+            System.out.println(main_string.substring(0, main_string.length()/2));
+        }
+    
+}
